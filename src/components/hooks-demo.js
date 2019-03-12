961067088b82e5b48a605bdd150e06162d22091a
Michael Wanyoike <micwanyoike@gmail.com>
2019-03-12 10:48:36 +0300

    Refactored form input

--- src/components/hooks-demo.js
+++ src/components/hooks-demo.js
@@ -1,68 +1,67 @@
 import React, { useState, useEffect } from "react";
 
 export default function HookDemo(props) {
-  const [name, setName] = useState("Agata");
-  const [location, setLocation] = useState("Nairobi");
-  const [resolution, setResolution] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight
-  });
-
-  function handleNameChange(e) {
-    setName(e.target.value);
-  }
-
-  function handleLocationChange(e) {
-    setLocation(e.target.value);
-  }
-
-  useEffect(() => {
-    document.title = name + " from " + location;
-  });
-
-  useEffect(() => {
-    const handleResize = () => {
-      setResolution({
-        width: window.innerWidth,
-        height: window.innerHeight
-      });
-    };
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize ", handleResize);
-    };
-  });
+  const name = useFormInput("Agata");
+  const location = useFormInput("Nairobi");
+  const resolution = useWindowResolution();
+  useDocumentTitle(name.value + " from " + location.value);
 
   return (
     <section>
-      <form autocomplete="off">
+      <form autoComplete="off">
         <section>
           <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            value={name}
-            onChange={handleNameChange}
-          />
+          <input {...name} />
         </section>
         <section>
           <label htmlFor="location">Location</label>
-          <input
-            type="text"
-            name="location"
-            id="location"
-            value={location}
-            onChange={handleLocationChange}
-          />
+          <input {...location} />
         </section>
       </form>
       <p>
-        Hello {name} from {location}
+        Hello {name.value} from {location.value}
       </p>
       <h3>
         {resolution.width} x {resolution.height}
       </h3>
     </section>
   );
+}
+
+function useDocumentTitle(title) {
+  useEffect(() => {
+    document.title = title;
+  });
+}
+
+function useFormInput(initialValue) {
+  const [value, setValue] = useState(initialValue);
+
+  function handleChange(e) {
+    setValue(e.target.value);
+  }
+
+  return {
+    value,
+    onChange: handleChange
+  };
+}
+
+function useWindowResolution() {
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+      setHeight(window.innerHeight);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize ", handleResize, true);
+    };
+  }, [width, height]);
+  return {
+    width,
+    height
+  };
 }
