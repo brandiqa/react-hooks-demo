70d941b65ab86faa6626eadc1ece7332e3f6c4b0
Michael Wanyoike <micwanyoike@gmail.com>
2020-09-21 13:19:51 +0300

    Updated packages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+// import { Container, Row, Col } from "react-bootstrap";
 import HookDemo from "./components/hooks-demo";
 // import ClassDemo from "./components/class-demo";
 import "./App.css";
@@ -7,26 +7,32 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Container>
-          <Row>
-            <Col sm={12}>
-              <h1>Hook Demo</h1>
-            </Col>
-            <Col sm={12}>
-              <HookDemo />
-            </Col>
-          </Row>
-          {/* <Row>
-            <Col sm={12}>
-              <h1>Class Demo</h1>
-            </Col>
-            <Col sm={12}>
-              <ClassDemo />
-            </Col>
-          </Row> */}
-        </Container>
+      <div className="container">
+        <section>
+          <h1>Hook Demo</h1>
+          <HookDemo />
+        </section>
       </div>
+      // <div className="App">
+      //   <Container>
+      //     <Row>
+      //       <Col sm={12}>
+      //         <h1>Hook Demo</h1>
+      //       </Col>
+      //       <Col sm={12}>
+      //         <HookDemo />
+      //       </Col>
+      //     </Row>
+      //     <Row>
+      //       <Col sm={12}>
+      //         <h1>Class Demo</h1>
+      //       </Col>
+      //       <Col sm={12}>
+      //         <ClassDemo />
+      //       </Col>
+      //     </Row>
+      //   </Container>
+      // </div>
     );
   }
 }
